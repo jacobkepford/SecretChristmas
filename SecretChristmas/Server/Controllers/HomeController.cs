663386338125f41111568652474705a0761a5072
Jacob Kepford <rtbredge@gmail.com>
2021-11-20 19:43:47 -0600

    Created edit party page.

--- SecretChristmas/Server/Controllers/HomeController.cs
+++ SecretChristmas/Server/Controllers/HomeController.cs
@@ -9,45 +9,51 @@ namespace SecretChristmas.Server.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private static PartyGuest host1 = new()
+        {
+            PartyGuestID = 1,
+            FirstName = "Jacob",
+            LastName = "Kepford",
+        };
+
+        private static PartyGuest host2 = new()
+        {
+            PartyGuestID = 2,
+            FirstName = "Bailey",
+            LastName = "Wiles"
+        };
+
+        private static PartyGuest host3 = new()
+        {
+            PartyGuestID = 1,
+            FirstName = "Marty",
+            LastName = "Cat"
+        };
+
+        private static List<Party> PartyList = new()
+        {
+            new Party { PartyID = 1, PartyName = "Kepford Christmas", PartyHost = host1, PartyDate = new DateTime(2021, 12, 25) },
+            new Party { PartyID = 2, PartyName = "Wiles Wonderland", PartyHost = host2, PartyDate = new DateTime(2021, 12, 15) },
+            new Party { PartyID = 3, PartyName = "Marty Grinchmas", PartyHost = host3, PartyDate = new DateTime(2021, 12, 12) }
+        };
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
         }
 
         [HttpGet]
         public async Task<List<Party>> Get()
         {
-            PartyGuest host1 = new()
-            {
-                PartyGuestID = 1,
-                FirstName = "Jacob",
-                LastName = "Kepford",
-            };
-
-            PartyGuest host2 = new()
-            {
-                PartyGuestID = 2,
-                FirstName = "Bailey",
-                LastName = "Wiles"
-            };
-
-            PartyGuest host3 = new()
-            {
-                PartyGuestID = 1,
-                FirstName = "Marty",
-                LastName = "Cat"
-            };
-
-
-
-            List<Party> PartyList = new()
-            {
-                new Party { PartyID = 1, PartyName = "Kepford Christmas", PartyHost = host1, PartyDate = new DateTime (2021, 12, 25)},
-                new Party { PartyID = 2, PartyName = "Wiles Wonderland", PartyHost = host2, PartyDate = new DateTime(2021, 12, 15) },
-                new Party { PartyID = 3, PartyName = "Marty Grinchmas", PartyHost = host3, PartyDate = new DateTime(2021, 12, 12) }
-            };
-
             return PartyList;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Party> Get(int id)
+        {
+            Party selectedParty = PartyList.FirstOrDefault(s => s.PartyID == id);
+            return selectedParty;
         }
     }
 }
